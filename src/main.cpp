6f53cfb31fe8eb96cb48eceadf714fd69825d24a
Geoff McLane <ubuntu@geoffair.info>
2018-05-05 20:19:25 +0200

    Is #2 - Add user -H file for html header

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 	See the provided LICENSE.TXT file for details.
 */
 
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <iostream>
 #include <sstream>
 #include <fstream>
@@ -19,6 +21,39 @@
 using std::cerr;
 using std::cout;
 using std::endl;
+
+//////////////////////////////////////////////////////
+/// utils
+#ifdef _WIN32
+#define M_IS_DIR _S_IFDIR
+#else // !_WIN32
+#define M_IS_DIR S_IFDIR
+#endif
+
+#define MDT_NONE 0
+#define MDT_FILE 1
+#define MDT_DIR  2
+
+static struct stat buf;
+static int is_file_or_directory(const char * path)
+{
+    if (!path)
+        return MDT_NONE;
+    if (stat(path, &buf) == 0)
+    {
+        if (buf.st_mode & M_IS_DIR)
+            return MDT_DIR;
+        else
+            return MDT_FILE;
+    }
+    return MDT_NONE;
+}
+
+static size_t get_last_file_size() { return buf.st_size; }
+//////////////////////////////////////////////////////
+
+static const char *headFile = 0;
+static char *headContents = 0;
 
 namespace {
 
@@ -86,6 +121,48 @@ namespace {
                         cerr << "Option " << arg << " must be followed by output file, use -? for help." << endl;
                     }
                     break;
+                case 'H':   /* get users HTML HEADER file */
+                    if (x2 < argc) {
+                        x++;
+                        sarg = argv[x];
+                        if (is_file_or_directory(sarg) == MDT_FILE) {
+                            if (headFile) {
+                                err = true;
+                                cerr << "Too many parameters. Already had out '" << headFile
+                                    << "', found '" << arg << " " << sarg << "' too. Use -? for help." << endl;
+                            }
+                            else {
+                                size_t len = get_last_file_size();
+                                headContents = new char[len+1];    // allocate memory for a buffer of appropriate dimension
+                                if (!headContents) {
+                                    err = true;
+                                    cerr << "Error: Unable to allocate " << (len+1) << " bytes!" << endl;
+                                    return 1;
+                                }
+                                memset(headContents, 0, len + 1);   /* is this really required? But have 'seen' some rubbish! */
+                                std::ifstream t;
+                                t.open(sarg);
+                                if (t.bad()) {
+                                    err = true;
+                                    cerr << "Error: Unable to 'open' " << sarg << endl;
+                                    return 1;
+                                }
+                                t.read(headContents, len);    // read the whole file into the buffer
+                                t.close();                    // close file handle
+                                headContents[len] = 0;
+                                headFile = sarg;
+                            }
+                        }
+                        else {
+                            err = true;
+                            cerr << "Error: Unable to 'stat' file " << sarg << endl;
+                        }
+                    }
+                    else {
+                        err = true;
+                        cerr << "Option " << arg << " must be followed by html header file, use -? for help." << endl;
+                    }
+                    break;
                 default:
                     err = true;
                     cerr << "Unknown option " << arg << ", use -? for help." << endl;
@@ -126,6 +203,7 @@ namespace {
             "    -?, --help      Show this screen, and exit(0).\n"
             "    -d, --debug     Show tokens instead of HTML output.\n"
             "    -o, --out file  Write output to this file.\n"
+            "    -H, --HEAD file Get 'doctype' and <head>...</head> from file.\n"
             "\n"
             "  This program converts input (from an input file or stdin) from Markdown syntax\n"
             "  to HTML using the 'cppmarkdown' library.\n";
@@ -211,7 +289,13 @@ int main(int argc, char *argv[])
     else {
         std::stringstream ss;
         std::string s;
-        s = cDefHead;
+        if (headContents) {
+            s = headContents;
+            delete headContents;
+        }
+        else {
+            s = cDefHead;
+        }
         s += "<body>\n";
         doc.write(ss);  // add the body html
         s += ss.str();  // add to output
